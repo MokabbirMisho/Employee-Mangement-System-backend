9f3fb46888214afe96ec1ca07e547dd3cac6d414
MokabbirMisho <mokabbirmisho@gmail.com>
2025-11-25 05:15:44 +0100

    server connect

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // serve local uploads
 app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
